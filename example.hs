505b962d0def3b3108b4179397a5072bd0c7e11e
Bas van Dijk <v.dijk.bas@gmail.com>
2012-11-24 16:15:23 +0100

    Use usb-1.2

--- example.hs
+++ example.hs
@@ -5,6 +5,7 @@ module Main where
 -- from base:
 import System.IO
 import System.Exit
+import Data.Functor
 import Data.List
 import Control.Monad
 import Text.Printf
@@ -17,48 +18,58 @@ import Data.Function.Unicode ( (∘) )
 -- from bytestring:
 import qualified Data.ByteString as B ( ByteString, length, unpack )
 
+-- from vector:
+import           Data.Vector      ( (!) )
+import qualified Data.Vector as V ( toList )
+
 -- from usb:
 import System.USB
 
 main ∷ IO ()
 main = do
+  -- Initialization:
   ctx ← newCtx
   setDebug ctx PrintInfo
 
-  devs ← getDevices ctx
-  case find isMyMouse devs of
+  -- Enumerating devices & finding the right device:
+  devs ← V.toList <$> getDevices ctx
+  deviceDescs <- mapM getDeviceDesc devs
+  case fmap fst $ find (isMyMouse . snd) $ zip devs deviceDescs of
     Nothing → hPutStrLn stderr "Mouse not found" >> exitFailure
-    Just dev → withDeviceHandle dev $ \devHndl →
-      withDetachedKernelDriver devHndl 0 $
-        withClaimedInterface devHndl 0 $ do
+    Just dev →
 
-          let [config0]    = deviceConfigs $ deviceDesc dev
-              [interface0] = configInterfaces config0
-              [alternate0] = interface0
-              [endpoint1]  = interfaceEndpoints alternate0
-              mps          = maxPacketSize $ endpointMaxPacketSize endpoint1
+      -- Opening the device:
+      withDeviceHandle dev $ \devHndl →
+        withDetachedKernelDriver devHndl 0 $
+          withClaimedInterface devHndl 0 $ do
 
-              nrOfBytesToRead = 20 * mps
+            -- Inspecting descriptors:
+            config0 <- getConfigDesc dev 0
+            let interface0 = configInterfaces config0 ! 0
+                alternate0 = interface0 ! 0
+                endpoint1  = interfaceEndpoints alternate0 ! 0
+                mps        = maxPacketSize $ endpointMaxPacketSize endpoint1
 
-              timeout = 5000
+                nrOfBytesToRead = 20 * mps
 
-          _ ← printf "Reading %i bytes during a maximum of %i ms...\n"
-                      nrOfBytesToRead timeout
+                timeout = 5000
 
-          (bs, status) ← readInterrupt devHndl
-                                       (endpointAddress endpoint1)
-                                       nrOfBytesToRead
-                                       timeout
+            -- Performing I/O:
+            _ ← printf "Reading %i bytes during a maximum of %i ms...\n"
+                        nrOfBytesToRead timeout
 
-          when (status ≡ TimedOut) $ putStrLn "Reading timed out!"
-          _ ← printf "Read %i bytes:\n" $ B.length bs
-          printBytes bs
+            (bs, status) ← readInterrupt devHndl
+                                         (endpointAddress endpoint1)
+                                         nrOfBytesToRead
+                                         timeout
 
-isMyMouse ∷ Device → Bool
-isMyMouse dev = deviceVendorId  devDesc ≡ 0x045e
-              ∧ deviceProductId devDesc ≡ 0x0040
-    where
-      devDesc = deviceDesc dev
+            when (status ≡ TimedOut) $ putStrLn "Reading timed out!"
+            _ ← printf "Read %i bytes:\n" $ B.length bs
+            printBytes bs
+
+isMyMouse ∷ DeviceDesc → Bool
+isMyMouse devDesc = deviceVendorId  devDesc ≡ 0x045e
+                  ∧ deviceProductId devDesc ≡ 0x0040
 
 printBytes ∷ B.ByteString → IO ()
 printBytes = putStrLn ∘ intercalate " " ∘ map show ∘ B.unpack
